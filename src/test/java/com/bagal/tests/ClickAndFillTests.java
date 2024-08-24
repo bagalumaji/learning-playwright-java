3b23c87813f837260df902013a0924662bda4373
Umaji Bagal <bagalumaji@gmail.com>
2024-08-24 22:50:48 +0530

    added click test

--- src/test/java/com/bagal/tests/ClickAndFillTests.java
+++ src/test/java/com/bagal/tests/ClickAndFillTests.java
@@ -1,6 +1,7 @@
 package com.bagal.tests;
 
 import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.AriaRole;
 import org.junit.jupiter.api.Test;
 
 public class ClickAndFillTests {
@@ -11,11 +12,24 @@ public class ClickAndFillTests {
             BrowserContext browserContext = browser.newContext();
             Page page = browserContext.newPage();
             page.navigate("https://the-internet.herokuapp.com/inputs");
-            page.locator("").fill("test data");
+            page.locator("//input[@type=\"number\"]").fill("123");
             Thread.sleep(5000);
             page.close();
             browserContext.close();
             browser.close();
+        }
+    }
+    @Test
+    void clickTest() throws InterruptedException {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false));
+            BrowserContext browserContext = browser.newContext();
+            Page page = browserContext.newPage();
+            page.navigate("https://the-internet.herokuapp.com/add_remove_elements/");
+            page.getByRole(AriaRole.BUTTON,new Page.GetByRoleOptions().setName("Add Element")).click();
+            Thread.sleep(5000);
+            page.close();
+            browserContext.close();
         }
     }
 }
