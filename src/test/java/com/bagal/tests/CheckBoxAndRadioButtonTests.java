5e43b1665fc6f3d4174b30bd19cad245d4153960
Umaji Bagal <bagalumaji@gmail.com>
2024-08-25 13:48:48 +0530

    added test - check box

--- src/test/java/com/bagal/tests/CheckBoxAndRadioButtonTests.java
+++ src/test/java/com/bagal/tests/CheckBoxAndRadioButtonTests.java
@@ -1,10 +1,32 @@
 package com.bagal.tests;
 
+import com.microsoft.playwright.*;
 import org.junit.jupiter.api.Test;
+import com.microsoft.playwright.assertions.*;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class CheckBoxAndRadioButtonTests {
     @Test
     public void testCheckBoxTest() {
+        try(Playwright playwright = Playwright.create()){
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setChannel("chrome"));
+            BrowserContext browserContext = browser.newContext();
+            Page page = browserContext.newPage();
+            page.navigate("https://www.lambdatest.com/selenium-playground/checkbox-demo");
 
+            Locator clickOnCheckBox = page.getByLabel("Click on check box");
+          //  Thread.sleep(5000);
+            assertThat(clickOnCheckBox).isChecked();
+            clickOnCheckBox.uncheck();
+
+            //Thread.sleep(3000);
+            clickOnCheckBox.check();
+            //Thread.sleep(5000);
+
+            page.close();
+            browserContext.close();
+            browser.close();
+        }
     }
 }
