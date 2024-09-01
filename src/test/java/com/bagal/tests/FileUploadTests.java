284bc8623a22c3079eb41851e8bf03a09e3842cf
Umaji Bagal <bagalumaji@gmail.com>
2024-09-01 07:32:37 +0530

    file upload

--- src/test/java/com/bagal/tests/FileUploadTests.java
+++ src/test/java/com/bagal/tests/FileUploadTests.java
@@ -1,4 +1,28 @@
 package com.bagal.tests;
 
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.nio.file.Paths;
+
 public class FileUploadTests {
+    @Test
+    public void testFileUpload() {
+        try (Playwright playwright = Playwright.create()) {
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setChannel("chrome"));
+            BrowserContext browserContext = browser.newContext();
+            Page page = browserContext.newPage();
+
+            page.navigate("https://the-internet.herokuapp.com/upload");
+            Locator dragAndDropFile = page.locator("id=drag-drop-upload");
+            FileChooser fileChooser = page.waitForFileChooser(dragAndDropFile::click);
+            fileChooser.setFiles(Paths.get(System.getProperty("user.dir")+ File.separator+"src/test/resources/testdata/test.txt"));
+            page.waitForTimeout(1000);
+
+            page.close();
+            browserContext.close();
+            browser.close();
+        }
+    }
 }
